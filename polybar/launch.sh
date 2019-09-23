55e312ddbee3f85d7a5089bc44f3074476112833
Tyler Wong <tbwong3@gmail.com>
2019-09-22 17:06:50 -0700

    Initial commit

---
+++ polybar/launch.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the process have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch
+polybar top
+
+echo "Bars launched..."
+
